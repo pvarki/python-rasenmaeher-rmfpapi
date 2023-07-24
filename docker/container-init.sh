7921fc5543bcd2a66d86d05b90645143b07ae662
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-07-24 21:00:39 +0300

    call the init helper to get our client cert

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -4,6 +4,8 @@ then
   echo "First run already cone"
 else
   set -e
-  /kw_product_init help
+  /kw_product_init init /pvarki/kraftwerk-init.json
+  # the ready endpoint does not exist yet so this will always fail
+  # /kw_product_init ready /pvarki/kraftwerk-init.json
   date -u +"%Y%m%dT%H%M" >/data/persistent/firstrun.done
 fi

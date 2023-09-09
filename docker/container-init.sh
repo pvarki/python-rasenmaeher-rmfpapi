d189317bdf425257e6bed583ae18d7a8883671e3
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-09 12:46:34 +0300

    use intranet name for localmaeher

--- docker/container-init.sh
+++ docker/container-init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
+set -e
+# Resolve our magic names to docker internal ip
+sed 's/.*localmaeher.*//g' /etc/hosts >/etc/hosts.new && cat /etc/hosts.new >/etc/hosts
+echo "$(getent hosts rmnginx | awk '{ print $1 }') localmaeher.pvarki.fi mtls.localmaeher.pvarki.fi" >>/etc/hosts
+cat /etc/hosts
 if [ -f /data/persistent/firstrun.done ]
 then
   echo "First run already cone"
 else
-  set -e
-  # Resolve our magic names to docker internal ip
-  sed 's/.*localmaeher.*//g' /etc/hosts >/etc/hosts.new && cat /etc/hosts.new >/etc/hosts
-  echo "$(getent hosts host.docker.internal | awk '{ print $1 }') localmaeher.pvarki.fi mtls.localmaeher.pvarki.fi" >>/etc/hosts
   # Do the normal init
   /kw_product_init init /pvarki/kraftwerk-init.json
   # FIXME: This should be done natively in the FastAPI app

dd6f55386a84e73b0123981d1766614f4f93047c
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-09-09 13:15:56 +0300

    bump version

--- src/rmfpapi/__init__.py
+++ src/rmfpapi/__init__.py
@@ -1,2 +1,2 @@
 """ Fake product RASENMAEHER integration API service """
-__version__ = "0.2.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "0.2.1"  # NOTE Use `bump2version --config-file patch` to bump versions correctly

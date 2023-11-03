0ae4da7b8c6844963bad95e7461c534b4a9cf08e
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-11-03 20:42:30 +0200

    implement healthcheck api endpoint Update deps (need new libpvarki for the healthcheck response schema for example)

---
+++ src/rmfpapi/api/healthcheck.py
@@ -0,0 +1,17 @@
+"""Health-check endpooint(s)"""
+import logging
+
+from fastapi import APIRouter, Depends
+from libpvarki.middleware import MTLSHeader
+from libpvarki.schemas.product import ProductHealthCheckResponse
+
+
+LOGGER = logging.getLogger(__name__)
+
+router = APIRouter(dependencies=[Depends(MTLSHeader(auto_error=True))])
+
+
+@router.get("")
+async def request_healthcheck() -> ProductHealthCheckResponse:
+    """Check that we are healthy, return accordingly"""
+    return ProductHealthCheckResponse(healthy=True, extra="Dummy, nothing actually checked")

ea38b9296d824922315f6633b2c4109ef05dba01
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-01-29 23:11:15 +0200

    we have already tagged containers as 1.0.0

--- src/rmfpapi/__init__.py
+++ src/rmfpapi/__init__.py
@@ -1,2 +1,2 @@
 """ Fake product RASENMAEHER integration API service """
-__version__ = "0.5.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.1.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly

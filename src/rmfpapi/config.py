7f2dd06f5d12368fa9ce5126ef35016711e541a9
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-02-05 22:30:50 +0200

    drop .env file support due to starlette

--- src/rmfpapi/config.py
+++ src/rmfpapi/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from starlette.config import Config
 
-cfg = Config(".env")
+cfg = Config()  # not supporting .env files anymore because https://github.com/encode/starlette/discussions/2446
 
 LOG_LEVEL: int = cfg("LOG_LEVEL", default=20, cast=int)
 TEMPLATES_PATH: Path = cfg("TEMPLATES_PATH", cast=Path, default=Path(__file__).parent / "templates")

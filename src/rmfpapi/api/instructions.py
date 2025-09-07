d31a24ee849866ffaeb744d0cf4b6e0fb786cc42
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-09-07 03:16:02 +0300

    feat: handle prefixed RUNE assets

--- src/rmfpapi/api/instructions.py
+++ src/rmfpapi/api/instructions.py
@@ -4,20 +4,39 @@ import logging
 import json
 from pathlib import Path
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import FileResponse
 from libpvarki.middleware import MTLSHeader
 from libpvarki.schemas.product import UserCRUDRequest
+
+from ..config import load_manifest
 
 LOGGER = logging.getLogger(__name__)
 
 router = APIRouter(dependencies=[Depends(MTLSHeader(auto_error=True))])
 
 
+@router.get("/assets/{file_path:path}")
+async def get_asset(file_path: str) -> FileResponse:
+    """Asset file"""
+    basepath = Path("/opt/templates/assets")
+    assetpath = basepath / file_path
+    LOGGER.info("Looking for {} from {}".format(file_path, assetpath))
+    if not assetpath.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+    return FileResponse(path=str(assetpath))
+
+
 @router.post("/{language}")
 async def user_intructions(user: UserCRUDRequest, language: str) -> Dict[str, str]:
     """return user instructions"""
     instructions_json_file = Path("/opt/templates/rune-fake.json")
+    manifest = load_manifest()
 
-    tak_instructions_data = json.loads(instructions_json_file.read_text(encoding="utf-8"))
+    instructions_text = instructions_json_file.read_text(encoding="utf-8")
+    instructions_text = instructions_text.replace(
+        "__FAKE_ASSETS__", f"{manifest['product']['api']}api/v1/instructions/assets"
+    )
+    instructions_data = json.loads(instructions_text)
 
-    return {"callsign": user.callsign, "instructions": json.dumps(tak_instructions_data), "language": language}
+    return {"callsign": user.callsign, "instructions": json.dumps(instructions_data), "language": language}
--- src/rmfpapi/config.py
+++ src/rmfpapi/config.py
@@ -1,9 +1,35 @@
 """Configurations with .env support"""
+from typing import Dict, Any, cast
 from pathlib import Path
+import functools
+import json
+import logging
 
 from starlette.config import Config
+
+LOGGER = logging.getLogger(__name__)
 
 cfg = Config()  # not supporting .env files anymore because https://github.com/encode/starlette/discussions/2446
 
 LOG_LEVEL: int = cfg("LOG_LEVEL", default=20, cast=int)
 TEMPLATES_PATH: Path = cfg("TEMPLATES_PATH", cast=Path, default=Path(__file__).parent / "templates")
+
+
+@functools.cache
+def load_manifest(filepth: Path = Path("/pvarki/kraftwerk-init.json")) -> Dict[str, Any]:
+    """Load the manifest"""
+    if not filepth.exists():
+        # return a dummy manifest
+        LOGGER.warning("Returning dummy manifest")
+        rm_uri = "https://localmaeher.dev.pvarki.fi"
+        mtls_uri = rm_uri.replace("https://", "https://mtls.")
+        return {
+            "deployment": "localmaeher",
+            "rasenmaeher": {
+                "init": {"base_uri": rm_uri, "csr_jwt": "LOL, no"},
+                "mtls": {"base_uri": mtls_uri},
+                "certcn": "rasenmaeher",
+            },
+            "product": {"dns": "fake.localmaeher.dev.pvarki.fi"},
+        }
+    return cast(Dict[str, Any], json.loads(filepth.read_text(encoding="utf-8")))

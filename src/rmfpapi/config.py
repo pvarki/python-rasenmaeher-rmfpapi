90dd3cb43f8b63243e46c9a55219cd10dbe7bcbc
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-11-25 13:55:48 +0200

    feat: get product manifest (or dummy if in testing)

--- src/rmfpapi/config.py
+++ src/rmfpapi/config.py
@@ -1,5 +1,8 @@
 """Configurations with .env support"""
+from typing import Dict, Any, cast
 from pathlib import Path
+import json
+import functools
 
 from starlette.config import Config
 
@@ -7,3 +10,25 @@ cfg = Config()  # not supporting .env files anymore because https://github.com/e
 
 LOG_LEVEL: int = cfg("LOG_LEVEL", default=20, cast=int)
 TEMPLATES_PATH: Path = cfg("TEMPLATES_PATH", cast=Path, default=Path(__file__).parent / "templates")
+
+
+@functools.cache
+def get_manifest() -> Dict[str, Any]:
+    """Get manifest contents"""
+    pth = Path("/pvarki/kraftwerk-init.json")
+    if not pth.exists():
+        return {
+            "deployment": "manifest_notfound",
+            "rasenmaeher": {
+                "init": {"base_uri": "https://localmaeher.dev.pvarki.fi:4439/", "csr_jwt": ""},
+                "mtls": {"base_uri": "https://mtls.localmaeher.dev.pvarki.fi:4439/"},
+                "certcn": "rasenmaeher",
+            },
+            "product": {
+                "dns": "fake.localmaeher.dev.pvarki.fi",
+                "api": "https://fake.localmaeher.dev.pvarki.fi:4626/",
+                "uri": "https://fake.localmaeher.dev.pvarki.fi:4626/",
+            },
+        }
+    data = json.loads(pth.read_text(encoding="utf-8"))
+    return cast(Dict[str, Any], data)

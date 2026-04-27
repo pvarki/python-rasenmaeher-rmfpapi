97d871bd324e69ffd937b05e9bfd97ffa16d632d
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-04-27 15:07:30 +0300

    chore: migrate to bump-my-version

--- src/rmfpapi/__init__.py
+++ src/rmfpapi/__init__.py
@@ -1,3 +1,3 @@
 """Fake product RASENMAEHER integration API service"""
 
-__version__ = "1.4.2"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "1.4.2+260427"  # NOTE Use `bump-my-version` to bump versions correctly

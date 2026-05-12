dee0fdbf136b1376ab979e9a0268204c1c5c7222
Akseli Lehtniemi <60463103+Akaseli@users.noreply.github.com>
2026-05-12 10:35:59 +0300

    chore: bump version

--- src/rmfpapi/__init__.py
+++ src/rmfpapi/__init__.py
@@ -1,3 +1,3 @@
 """Fake product RASENMAEHER integration API service"""
 
-__version__ = "1.4.2+260427"  # NOTE Use `bump-my-version` to bump versions correctly
+__version__ = "1.5.0+260512"  # NOTE Use `bump-my-version` to bump versions correctly
--- tests/test_rmfpapi.py
+++ tests/test_rmfpapi.py
@@ -6,7 +6,7 @@ from rmfpapi import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.4.2+260427"
+    assert __version__ == "1.5.0+260512"
 
 
 def test_healthcheck(mtlsclient: TestClient) -> None:

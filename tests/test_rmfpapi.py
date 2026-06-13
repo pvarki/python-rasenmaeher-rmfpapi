89116c7e8831be90b59338019f36dfb869585f5b
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-06-13 15:09:04 +0300

    chore: bump version

--- tests/test_rmfpapi.py
+++ tests/test_rmfpapi.py
@@ -6,7 +6,7 @@ from rmfpapi import __version__
 
 def test_version() -> None:
     """Make sure version matches expected"""
-    assert __version__ == "1.4.2"
+    assert __version__ == "1.4.3"
 
 
 def test_healthcheck(mtlsclient: TestClient) -> None:
